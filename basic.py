e43d3eb991617df5b2dd874f69a792922248b78b
Mintass <ashf293@163.com>
2023-07-08 16:10:57 +0800

    book link

--- basic.py
+++ basic.py
@@ -33,6 +33,23 @@ def not_master_vocabulary_book(text):
         text.insert(tk.END, f.read())
 
 def save_file(text):
-    if file_path:
-        with open(file_path, 'w') as f:
-            f.write(text.get('1.0', tk.END))
+    if not file_path:
+        return
+    
+    origin_content = get_set(file_path)
+    with open(file_path, 'w') as f:
+        f.write(text.get('1.0', tk.END))
+    new_content = get_set(file_path)
+    
+    if origin_content == new_content:
+        return
+    reduce_word_set = origin_content - new_content
+    add_word_set = new_content - origin_content
+    
+    other_file_path = 'master_word.txt' if file_path == 'not_master_word.txt' else 'not_master_word.txt'
+    other_word_set = get_set(other_file_path)
+    other_word_set |= reduce_word_set
+    other_word_set -= add_word_set
+    other_word_str = ' '.join(sorted(other_word_set))
+    with open(other_file_path, 'w') as f:
+        f.write(other_word_str)
